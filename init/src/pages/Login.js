d42f87325c9c6d7fa4ff594ac1d1514a0a9dae6b
Labh Bothra <labhbothra1380@gmail.com>
2025-05-14 14:11:13 +0530

    D

--- init/src/pages/Login.js
+++ init/src/pages/Login.js
@@ -8,7 +8,7 @@ const LoginContainer = styled.div`
   justify-content: center;
   align-items: center;
   min-height: 100vh;
-  background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
+  background: linear-gradient(135deg, #FFD60E 0%, #FD241D 100%);
   padding: 20px;
 `;
 
